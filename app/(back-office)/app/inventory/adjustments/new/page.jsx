5d520355a93d8de70d53c8a922207e06682f3608
abdulmaleekadams <harbdulmaleek@gmail.com>
2024-01-15 09:16:28 +0100

    Update Adjustment/new/page.jsx (render form dynamically)

--- app/(back-office)/app/inventory/adjustments/new/page.jsx
+++ app/(back-office)/app/inventory/adjustments/new/page.jsx
@@ -1,34 +1,39 @@
-import AddInventoryForm from '@/components/inventory/AddInventoryForm copy';
+'use client';
+import AddInventoryForm from '@/components/inventory/AddInventoryForm';
 import FormHeader from '@/components/inventory/FormHeader';
 import InventoryNav from '@/components/inventory/FormInputs/InventoryNav';
 import TransferInventoryForm from '@/components/inventory/TransferInventoryForm';
 import { Plus, Minus } from 'lucide-react';
+import { useState } from 'react';
 
 const tabs = [
   {
     title: 'Add Stock',
     icon: Plus,
+    formToDisplay: 'add',
   },
   {
     title: 'Transfer Stock',
     icon: Minus,
+    formToDisplay: 'transfer',
   },
 ];
 
 const NewIAdjustments = () => {
+  const [activeForm, setActiveForm] = useState('add');
   return (
     <div>
       {/* Header */}
       <FormHeader title={'New Adjustment'} url={'/app/inventory/items'} />
 
-      
-      <InventoryNav tabs={tabs} />
+      <InventoryNav
+        tabs={tabs}
+        activeForm={activeForm}
+        setActiveForm={setActiveForm}
+      />
 
       {/* Form */}
-      <TransferInventoryForm />
-      <AddInventoryForm />
-
-      {/*  */}
+      {activeForm === 'add' ? <AddInventoryForm /> : <TransferInventoryForm />}
     </div>
   );
 };
