d344a309a9e1a18543db02f3d28dafb8ab48e68a
abdulmaleekadams <harbdulmaleek@gmail.com>
2024-07-15 10:32:07 +0100

    Update next to latest version

--- app/page.js
+++ app/page.js
@@ -3,8 +3,7 @@ export default function Home() {
   return (
     <div className='flex items-center flex-col justify-center min-h-screen'>
       <h2 className='text-3xl mb-4'>Inventory Management Software</h2>
-      <Link href='/app/home/overview
-      '>View dashboard</Link>
+      <Link href='/app/home/inventory-dashboard'>View dashboard</Link>
     </div>
   );
 }
